92f92b1ae45395e19eb13d05fd831a76b443de86
Asser Hamad <asserhamad@gmail.com>
2020-09-29 11:36:58 +0200

    adding our team

--- client/src/Components/Homepage/Homepage.js
+++ client/src/Components/Homepage/Homepage.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import './Homepage.css';
+import OurTeam from './OurTeam/OurTeam';
 
 function Homepage(){
     useEffect(() => {document.getElementsByTagName('video')[0].volume = 0.2},[])
     return(
         <div className="home-container">
             <div className="home-video-container">
-                <video width="100%" height="100%" src="https://res.cloudinary.com/duiexwi8t/video/upload/v1601366162/Jazan/jazan_fhkluc.mp4" autoPlay loop/>
+                <video width="100%" height="100%" src="https://res.cloudinary.com/duiexwi8t/video/upload/v1601366162/Jazan/jazan_fhkluc.mp4" autoPlay loop muted/>
             </div>
             <div className="home-content-container">
+                <OurTeam />
             </div>
         </div>
     )
---
+++ client/src/Components/Homepage/OurTeam/OurTeam.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import './OurTeam.css';
+
+function OurTeam(){
+
+    return <div className="our-team-container">
+        <div className="our-team-text">
+            “A distinguished team of highly specialized consulting firms gathered their efforts to achieve the
+            aimed MISSION & to settle the anticipated VISIONS for Jazan Region and Jizan City”
+        </div>
+        <div className="our-team-title">
+            Our Team
+        </div>
+    </div>
+}
+
+export default OurTeam;
